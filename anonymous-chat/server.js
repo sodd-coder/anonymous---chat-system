49af06b407e66b3f09c1fca5a63647347df29a64
sodd-coder <somashekhargururaj@email.com>
2026-03-17 22:39:53 +0530

    making app

--- anonymous-chat/server.js
+++ anonymous-chat/server.js
@@ -64,7 +64,7 @@ io.on('connection', (socket) => {
 
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
